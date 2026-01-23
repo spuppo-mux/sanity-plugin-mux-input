623cc0a85d175a78bb06336602dc7536d3649343
Santiago Puppo <spuppo@mux.com>
2026-01-23 12:10:48 -0300

    Refactored metadata and file size into hooks and added drm audio-only validation

---
+++ src/hooks/useMediaMetadata.ts
@@ -0,0 +1,97 @@
+import {useEffect, useState} from 'react'
+
+import {StagedUpload} from '../components/Uploader'
+
+export interface VideoAssetMetadata {
+  width?: number
+  height?: number
+  duration?: number
+  size?: number
+}
+
+export function useMediaMetadata(stagedUpload: StagedUpload) {
+  const [videoAssetMetadata, setVideoAssetMetadata] = useState<VideoAssetMetadata | null>(null)
+  const [isLoadingMetadata, setIsLoadingMetadata] = useState(false)
+  useEffect(() => {
+    let videoSrc = null
+    // Validate file uploads
+    if (stagedUpload.type === 'file') {
+      const file = stagedUpload.files[0]
+      videoSrc = URL.createObjectURL(file)
+    }
+
+    // Validate URL uploads
+    if (stagedUpload.type === 'url') {
+      videoSrc = stagedUpload.url
+    }
+
+    setVideoAssetMetadata((old) => ({
+      ...old,
+      duration: undefined,
+      width: undefined,
+      height: undefined,
+    }))
+
+    if (!videoSrc) return () => null
+
+    setIsLoadingMetadata(true)
+    const videoElement = document.createElement('video')
+    videoElement.preload = 'metadata'
+
+    const metadataListeners = [
+      () => {
+        setIsLoadingMetadata(false)
+      },
+      () => {
+        const duration = videoElement.duration
+        const width = videoElement.width
+        const height = videoElement.height
+        setVideoAssetMetadata((old) => {
+          return {
+            ...old,
+            duration: duration,
+            width: width,
+            height: height,
+          }
+        })
+      },
+    ]
+
+    const cleanupVideo = (videoEl: HTMLVideoElement) => {
+      const currentVideoSrc = videoEl?.src
+      if (videoEl) {
+        metadataListeners.forEach((listener) =>
+          videoEl.removeEventListener('loadedmetadata', listener)
+        )
+        videoEl.onerror = null
+        videoEl.src = ''
+        videoEl.load()
+      }
+      if (currentVideoSrc?.startsWith('blob:')) {
+        URL.revokeObjectURL(currentVideoSrc)
+      }
+    }
+    metadataListeners.push(() => setTimeout(() => cleanupVideo(videoElement), 0))
+
+    videoElement.onerror = () => {
+      setIsLoadingMetadata(false)
+      console.warn('Could not read video metadata for validation')
+      cleanupVideo(videoElement)
+    }
+
+    metadataListeners.forEach((listener) =>
+      videoElement.addEventListener('loadedmetadata', listener)
+    )
+    videoElement.src = videoSrc
+
+    return () => {
+      cleanupVideo(videoElement)
+    }
+  }, [stagedUpload.type, stagedUpload])
+
+  return {
+    videoAssetMetadata,
+    setVideoAssetMetadata,
+    isLoadingMetadata,
+  }
+}
